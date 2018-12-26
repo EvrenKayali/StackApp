061e0687c9ea2b4c56d291ebc0ea5512e98494ce
Evren Kayali <evren.kayali@readify.net>
2018-12-26 18:33:12 +1100

    initial commit

---
+++ src/StackApp.Web/Features/Home/HomeController.cs
@@ -0,0 +1,19 @@
+﻿namespace StackApp.Web.Features.Home
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using System.Reflection;
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var vm = new HomeViewModel
+            {
+                HostName = Environment.MachineName,
+                Version = Assembly.GetExecutingAssembly().GetName().Version.ToString()
+            };
+            return View(vm);
+        }
+    }
+}
---
+++ src/StackApp.Web/Features/Home/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StackApp.Web.Features.Home
+{
+    public class HomeViewModel
+    {
+        public string HostName { get; set; }
+        public string Version { get; set; }
+    }
+}
